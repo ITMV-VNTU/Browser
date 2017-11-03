073218b443193bebd26519f7da11d1f0f8440d44
vitalii <2kn15b.bondarchuk@example.com>
2017-11-03 22:03:10 +0200

    h

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,10 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.post('/', function(req, res){
+  fetch('https://api.github.com/repos/facebook/react/issues',{method:'GET'}).then(function(response) {
+  //res.send(response);
+  console.log(response)
+  })
+})
 module.exports = router;
