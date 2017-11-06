a653b878d411e3be894f13c6a6e4ac9375fb93f7
vitalii <2kn15b.bondarchuk@example.com>
2017-11-06 21:55:20 +0200

    add issue

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var get_issue=require('./routes/get_issue');
 
 global.app = module.exports = express();
 //var multipart = require('connect-multiparty');
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/', get_issue);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
